7bc2867bc08044dc1302f89d56122991eb74c2cf
timwang2001 <501320394@qq.com>
2024-04-25 23:16:17 +0800

    print perceptual loss

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -52,7 +52,7 @@ class CycleGANModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B']
+        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A','perceptual_A', 'D_B', 'G_B', 'cycle_B', 'idt_B','perceptual_B']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
         visual_names_A = ['real_A', 'fake_B', 'rec_A']
         visual_names_B = ['real_B', 'fake_A', 'rec_B']
@@ -172,14 +172,14 @@ class CycleGANModel(BaseModel):
         # GAN loss D_B(G_B(B))
         self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
         # Forward cycle loss || G_B(G_A(A)) - A||
-        self.perceptualloss_A = self.criterionPerceptual(self.rec_A, self.real_A) * lambda_A
+        self.loss_perceptual_A = self.criterionPerceptual(self.rec_A, self.real_A) * lambda_A
         self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * lambda_A
         # Backward cycle loss || G_A(G_B(B)) - B||
         self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * lambda_B
         # combined loss and calculate gradients
-        self.perceptualloss_B = self.criterionPerceptual(self.rec_B, self.real_B) * lambda_B
+        self.loss_perceptual_B = self.criterionPerceptual(self.rec_B, self.real_B) * lambda_B
 
-        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B +self.perceptualloss_B +self.perceptualloss_A
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B +self.loss_perceptual_A +self.loss_perceptual_B
         self.loss_G.backward()
 
     def optimize_parameters(self):
